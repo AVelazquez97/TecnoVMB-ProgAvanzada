ac02140648523c54198c0494fba5544a4b09a525
AVelazquez97 <alexisvelazquez2012@gmail.com>
2023-06-14 13:28:54 -0300

    Se agrega fabrica, interfaz, iControlador, Controlador, enums, archivo de funciones, se modifica makefile, se agrega posible operación de modificar fecha

---
+++ iControlador/IControlador.h
@@ -0,0 +1,41 @@
+#ifndef ICONTROLADOR_H
+#define ICONTROLADOR_H
+#include <ctime>
+#include <iostream>
+
+// Se deben importar todos los datatypes a utilizar en las definiciones de las operaciones
+#include "../datatypes/headers/DTHabitacion.h"
+#include "../datatypes/headers/DTHostal.h"
+#include "../datatypes/headers/DTUsuario.h"
+
+#include "../enums/EnumCargo.h"
+#include "../enums/EnumEstado.h"
+#include "../enums/EnumTipo.h"
+
+using namespace std;
+
+// Esta sería la interfaz
+class IControlador {
+    private: 
+
+    public:
+        virtual ~IControlador();
+
+		// En esta sección se definen todas las operaciones que están definidas en el controlador. 
+		// Deben ser virtuales e igualadas a 0
+
+        // Usuarios
+        
+        // Hostales
+        
+        // Reservas
+        
+        // Estadías
+
+        //Cambiar fecha del sistema
+        virtual void setFechaSistema(tm*) = 0;
+        virtual time_t* getFechaSistema() = 0; 
+
+};
+
+#endif
