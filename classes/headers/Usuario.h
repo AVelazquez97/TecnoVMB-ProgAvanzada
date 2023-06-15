c4886ca09755da00bc4dfcf922e1a757e2137de1
Nacho <nachofernandezuru@gmail.com>
2023-06-14 21:19:24 -0300

    Subida
    @ Se agregan atributos a la clase Usuario

--- classes/headers/Usuario.h
+++ classes/headers/Usuario.h
@@ -1,6 +1,15 @@
 #ifndef USUARIO_H_
 #define USUARIO_H_
+#include<iostream>
+using namespace std;
 
+class Usuario{
+    protected:
+        string nombre;
+        string email;
+        string password;
+    public:
 
+};
 
 #endif // USUARIO_H_
